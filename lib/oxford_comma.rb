63dcdd4cb3c80aa40c10302bc175203284a44b20
Andrew Trent <andrewtrentdesign@gmail.com>
2018-09-28 22:24:39 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
+require 'pry'
 def oxford_comma(array)
+  if array.size == 1
+    return array[0]
 
+  elsif array.size == 2
+    return (array.join(" and "))
+
+  else
+    new_arr = array
+    new_arr.pop
+    str = new_arr.join(", ")
+    second_arr = [str,array.pop]
+    second_str = second_arr.join(", and ")
+    return second_str + "."
+  end
 end
