d53804fce3356d31efc41c999f497e0227a6e961
Andrew Trent <andrewtrentdesign@gmail.com>
2018-09-28 22:44:33 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,11 +7,7 @@ def oxford_comma(array)
     return (array.join(" and "))
 
   else
-    new_arr = array
-    new_arr[-1].delete
-    str = new_arr.join(", ")
-    second_arr = [str, array[-1]]
-    second_str = second_arr.join(", and ")
-    return second_str + "."
+    array[-1] = "and " + array[-1]
+    array.join(", ")
   end
 end
